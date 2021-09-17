7a2a3e620fcc1668e2853e22453ecedcfd63df0b
AidounNejma <guermachenejma@gmail.com>
2021-09-17 21:46:00 +0200

    Modifications page accueil
    J'ai encore quelques petites erreurs que je revérifierai demain sur le JS

--- assets/accueil.js
+++ assets/accueil.js
@@ -5,17 +5,22 @@ import './styles/accueil.css';
 function SlideShow(n) {
     var i;
 
+    var fade = document.getElementsByClassName("fade");
     var slides = document.getElementsByClassName("Containers");
     var circles = document.getElementsByClassName("dots");
     if (n > slides.length) {slidePosition = 1}
         if (n < 1) {slidePosition = slides.length}
         for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
+            slides[i].classList.toggle("fade");
+            slides[i].style.display = "none";
+        }
+
     for (i = 0; i < circles.length; i++) {
         circles[i].className = circles[i].className.replace(" enable", "");
     }
+
     slides[slidePosition-1].style.display = "block";
+    slides[i].classList.toggle("fade");
     circles[slidePosition-1].className += " enable";
 } 
 
@@ -25,6 +30,7 @@ SlideShow(slidePosition);
 // Contrôle Suivant/Précédent
 window.plusSlides = function (n) {
     SlideShow(slidePosition += n);
+
 }
 
 //  Contrôle des images
