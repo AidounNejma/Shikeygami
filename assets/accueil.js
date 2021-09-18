8cd70d944e3f3020c7e6395e837a19ecb5f0fce3
AidounNejma <guermachenejma@gmail.com>
2021-09-18 23:54:05 +0200

    Changement page accueil (contact)
    J'ai fait quelques modifs sur la section contact

--- assets/accueil.js
+++ assets/accueil.js
@@ -39,3 +39,47 @@ window.currentSlide= function(n) {
 }
 
 /**********************************************************/
+var phone = document.getElementsByClassName("phone");
+var address = document.getElementsByClassName("address");
+var mail = document.getElementsByClassName("mail");
+var facebook = document.getElementsByClassName("facebook");
+
+var question = document.getElementsByClassName("question");
+var googleMaps = document.getElementsByClassName("googleMaps");
+var contact = document.getElementsByClassName("contact");
+var contactForm = document.getElementsByClassName("contactForm");
+var facebookLink = document.getElementsByClassName("facebookLink");
+
+phone[0].addEventListener('click', showPhone);
+address[0].addEventListener('click', showAddress);
+mail[0].addEventListener('click', showMail);
+facebook[0].addEventListener('click', showFacebook);
+
+function showPhone(){
+    question[0].style.display = "none";
+    googleMaps[0].style.display = "none";
+    contactForm[0].style.display = "none";
+    facebookLink[0].style.display = "none";
+    contact[0].style.display = "block";
+}
+function showAddress(){
+    question[0].style.display = "none";
+    contact[0].style.display = "none";
+    contactForm[0].style.display = "none";
+    facebookLink[0].style.display = "none";
+    googleMaps[0].style.display = "block";
+}
+function showMail(){
+    question[0].style.display = "none";
+    googleMaps[0].style.display = "none";
+    contact[0].style.display = "none";
+    facebookLink[0].style.display = "none";
+    contactForm[0].style.display = "block";
+}
+function showFacebook(){
+    question[0].style.display = "none";
+    googleMaps[0].style.display = "none";
+    contact[0].style.display = "none";
+    contactForm[0].style.display = "none";
+    facebookLink[0].style.display = "block";
+}
