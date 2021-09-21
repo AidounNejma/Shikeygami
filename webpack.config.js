d99c1732e779d9ebe5e397a69599f9db1e03b6f0
Fabien Cador <fabiencador@gmail.com>
2021-09-21 17:00:06 +0200

    initial commit legals

--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,8 @@ Encore
     .addEntry('register', './assets/register.js')
     .addEntry('menu', './assets/menu.js')
     .addEntry('concept', './assets/concept.js')
+    .addEntry('legals', './assets/legals.js')
+    
     
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
