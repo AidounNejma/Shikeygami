ed448b5770c20c2fb837c47633cc781bb8204342
AidounNejma <guermachenejma@gmail.com>
2021-09-23 10:43:39 +0200

    modification carousel page game_show

--- assets/game_show.js
+++ assets/game_show.js
@@ -7,6 +7,8 @@ let forward = document.querySelector(".forward");
 
 let slides = document.getElementsByClassName("containers");
 let image = document.querySelector("#image");
+let image2 = document.querySelector("#image2");
+let image3 = document.querySelector("#image3");
 
 let numImage = 1;
 
@@ -19,21 +21,51 @@ setInterval(slideNext, 5000);
 
 function slideNext(){
     numImage += 1;
-        if(numImage === 3){ 
+        if(numImage === 4){ 
             numImage = 1; 
         }
         slides[0].classList.toggle("fade");
-        image.src = '{{ dossier_images ~ game.imageUrl' + numImage + ' }}';
+        
+        if(numImage === 1){
+            image.style.display = "block";
+            image2.style.display = "none";
+            image3.style.display = "none";
+        }
+        if(numImage === 2){
+            image2.style.display = "block";
+            image.style.display = "none";
+            image3.style.display = "none";
+        }
+        if(numImage === 3){
+            image3.style.display = "block";
+            image.style.display = "none";
+            image2.style.display = "none";
+        }
+
 }
 
 function slideBack() {
     numImage = numImage -1;
         if(numImage === 0){
-            numImage = 3;
+            numImage = 4;
         }
 
         slides[0].classList.toggle("fade");
-        image.src = '{{ dossier_images ~ game.imageUrl'+ numImage +' }}';
+        if(numImage === 1){
+            image.style.display = "block";
+            image2.style.display = "none";
+            image3.style.display = "none";
+        }
+        if(numImage === 2){
+            image2.style.display = "block";
+            image.style.display = "none";
+            image3.style.display = "none";
+        }
+        if(numImage === 3){
+            image3.style.display = "block";
+            image.style.display = "none";
+            image2.style.display = "none";
+        }
 } 
 
 /**********************************************************/
